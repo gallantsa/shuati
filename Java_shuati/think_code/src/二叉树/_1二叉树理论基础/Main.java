1a82ea40a175839b68a85fdee00264d792dc411a
gallantsa <gallantsa@163.com>
2023-10-02 10:01:44 +0800

    二叉树_二叉树理论基础

---
+++ Java_shuati/think_code/src/二叉树/_1二叉树理论基础/Main.java
@@ -0,0 +1,18 @@
+package 二叉树._1二叉树理论基础;
+
+public class Main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
