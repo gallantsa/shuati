bd42ac37d1b344f2353d1dca48f44f1e1298a90c
gallantsa <gallantsa@163.com>
2023-10-05 21:41:17 +0800

    二叉树_合并二叉树

---
+++ Java_shuati/think_code/src/二叉树/_21合并二叉树/_617_合并二叉树/Main.java
@@ -0,0 +1,33 @@
+package 二叉树._21合并二叉树._617_合并二叉树;
+
+import 二叉树.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
