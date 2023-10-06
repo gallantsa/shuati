0f2300e48e665cefbe131af276d6cf4290e0a673
gallantsa <gallantsa@163.com>
2023-10-07 00:10:14 +0800

    二叉树_二叉搜索树的最近公共祖先

---
+++ Java_shuati/think_code/src/二叉树/_28二叉搜索树的最近公共祖先/_235_二叉搜索树的最近公共祖先/Main.java
@@ -0,0 +1,24 @@
+package 二叉树._28二叉搜索树的最近公共祖先._235_二叉搜索树的最近公共祖先;
+
+import 二叉树.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+}
