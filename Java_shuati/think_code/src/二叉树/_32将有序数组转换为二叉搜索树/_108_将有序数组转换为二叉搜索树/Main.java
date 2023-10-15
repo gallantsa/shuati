a84337e45cff7bf44f2e8ebce0e767fe45e965ca
gallantsa <gallantsa@163.com>
2023-10-15 11:07:14 +0800

    二叉树_将有序数组转换为二叉搜索树

---
+++ Java_shuati/think_code/src/二叉树/_32将有序数组转换为二叉搜索树/_108_将有序数组转换为二叉搜索树/Main.java
@@ -0,0 +1,37 @@
+package 二叉树._32将有序数组转换为二叉搜索树._108_将有序数组转换为二叉搜索树;
+
+import 二叉树.TreeNode;
+
+public class Main {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return traversal(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode traversal(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = traversal(nums, left, mid - 1);
+        root.right = traversal(nums, mid + 1, right);
+
+        return root;
+    }
+}
