33dd88901b5ca5aee4383bbe6af8de85cec40a24
gallantsa <gallantsa@163.com>
2023-10-21 00:31:26 +0800

    贪心算法_K次取反后最大化的数组和

---
+++ Java_shuati/think_code/src/贪心算法/_9K次取反后最大化的数组和/_1005_K次取反后最大化的数组和/Main.java
@@ -0,0 +1,29 @@
+package 贪心算法._9K次取反后最大化的数组和._1005_K次取反后最大化的数组和;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Main {
+}
+
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int K) {
+        // 将数组按照绝对值大小从大到小排序，注意要按照绝对值的大小
+        nums = IntStream.of(nums)
+                .boxed()// 装箱
+                .sorted((o1, o2) -> Math.abs(o2) - Math.abs(o1))
+                .mapToInt(Integer::intValue).toArray(); // 拆箱
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            //从前向后遍历，遇到负数将其变为正数，同时K--
+            if (nums[i] < 0 && K > 0) {
+                nums[i] = -nums[i];
+                K--;
+            }
+        }
+        // 如果K还大于0，那么反复转变数值最小的元素，将K用完
+        if (K % 2 == 1) nums[len - 1] = -nums[len - 1];
+        return Arrays.stream(nums).sum();
+
+    }
+}
