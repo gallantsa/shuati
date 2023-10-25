bc2a7fdb12c8ec910631457d8ea693da9e90856c
gallantsa <gallantsa@163.com>
2023-10-25 23:33:28 +0800

    动态规划_爬楼梯_进阶版

---
+++ Java_shuati/think_code/src/动态规划/_22爬楼梯_进阶版/_70_爬楼梯/Main.java
@@ -0,0 +1,25 @@
+package 动态规划._22爬楼梯_进阶版._70_爬楼梯;
+
+public class Main {
+}
+
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] v = new int[2];
+        v[0] = 1;
+        v[1] = 2;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        // 和上一题一样
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < v.length; j++) {
+                if (i >= v[j]) {
+                    dp[i] += dp[i - v[j]];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
