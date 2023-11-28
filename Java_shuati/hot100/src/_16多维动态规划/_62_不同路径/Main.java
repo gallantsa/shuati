303ad9e5f7d83955c6a1a68f5e343e3e9b19f99f
gallantsa <gallantsa@163.com>
2023-11-28 19:21:00 +0800

    hot100_多维动态规划_不同路径

---
+++ Java_shuati/hot100/src/_16多维动态规划/_62_不同路径/Main.java
@@ -0,0 +1,25 @@
+package _16多维动态规划._62_不同路径;
+
+public class Main {
+}
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        // 初始化第一行和第一列
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
